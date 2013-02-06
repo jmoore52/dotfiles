f7dc72a5d0363c34beecb9110003f281b9f5475a
Hristo Asenov <hasenov@ccottons-MacBook-Pro.local>
2013-02-06 09:29:47 -0500

    added -f option to force relinking of files

--- setup.bash
+++ setup.bash
@@ -1,11 +1,26 @@
 #!/bin/bash
 
+force=false
+
+if [ $1 == "-f" ];
+then
+  force=true
+fi
+
 if [ ! -f ~/.tmux.conf ];
 then 
+  ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
+elif [ $force ];
+then
+  rm -i ~/.tmux.conf
   ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
 fi
 
 if [ ! -f ~/.vimrc ];
 then 
+  ln -s ~/dotfiles/vimrc ~/.vimrc
+elif [ $force ];
+then
+  rm -i ~/.vimrc
   ln -s ~/dotfiles/vimrc ~/.vimrc
 fi
