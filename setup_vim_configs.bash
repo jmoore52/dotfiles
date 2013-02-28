1d10722ddfe23fc82fe90ef2d6cc83f619918559
Hristo Asenov <hasenov@hctrl-hsa01.(none)>
2013-02-28 13:23:06 -0500

    modified vim configs file

--- setup_vim_configs.bash
+++ setup_vim_configs.bash
@@ -1,2 +1,15 @@
+#!/bin/bash
+
+if [ ! -d ~/.vim ];
+then
+  mkdir ~/.vim
+fi
+
 cd ~/.vim
-git clone https://github.com/kien/ctrlp.vim.git bundle/ctrlp.vim
+
+if [[ `hostname -s` = h*-hsa* ]];
+then
+  git clone ssh://hasenov@10.100.1.1/~/.vim/bundle/ctrlp.vim bundle/ctrlp.vim
+else
+  git clone https://github.com/kien/ctrlp.vim.git bundle/ctrlp.vim
+fi
