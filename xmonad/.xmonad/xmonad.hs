0ecf5f111f60726313630a4b9a748d0d30aae387
Hristo Asenov <hristo.s.asenov@gmail.com>
2014-03-14 13:50:29 -0500

    now able to paste in xmonad with Mod3Mask+y

--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -1,6 +1,9 @@
 import XMonad
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
+import XMonad.Util.Paste
+
+import qualified Data.Map        as M
 
 main = do
   xmonad =<< xmobar defaultConfig
@@ -8,5 +11,13 @@ main = do
     , layoutHook  = avoidStruts $ layoutHook defaultConfig
     , manageHook  = manageHook defaultConfig <+> manageDocks
     , modMask     = mod4Mask
+    , keys        = myKeys <+> keys defaultConfig
     , borderWidth = 3
     }
+
+------------------------------------------------------------------------
+---- Key bindings. Add, modify or remove key bindings here.
+----
+myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
+    -- X-selection-paste buffer
+    [ ((modm, xK_y), pasteSelection) ]
